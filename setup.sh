c06918f5de23f983b0f603d5091d999e6f0ee5ce
Lance Thompson <lthompson05@gmail.com>
2020-04-03 16:50:28 -0700

    update setup script

--- setup.sh
+++ setup.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 ################################################################### SETUP ########################################################################
+S="$1"
+while [ -h "$S" ]; do
+  D="$(cd -P "$(dirname "$S")" && pwd)" && S="$(readlink "$S")" && [[ $S != /* ]] && S="$D/$S"
+done
+_SCRIPT_DIR="$(cd -P "$(dirname "$S")" && pwd)"
 set -o errexit -o errtrace -o nounset
 ##################################################################################################################################################
 
@@ -11,11 +16,12 @@ BREW_FORMULAS=(
   pcre
 )
 
-if ! command -v jq >/dev/null 2>&1; then
+if ! command -v jq > /dev/null 2>&1; then
   BREW_FORMULAS+=(jq)
 fi
 
 echo "Installing required Homebrew formulas..."
 brew install "${BREW_FORMULAS[@]}"
 
-echo "To finish setup, add the dotfiles/bin directory to your PATH. You will most likely want to put that in your .bash_profile, eg 'export PATH=\"\${PATH}:<path_to_repo>/dotfiles/bin\"'"
+echo "To finish setup, add the dotfiles/bin directory to your PATH. You will most likely want to put that in your .bash_profile."
+echo "export PATH=\"\${PATH}:${_SCRIPT_DIR}/dotfiles/bin\""

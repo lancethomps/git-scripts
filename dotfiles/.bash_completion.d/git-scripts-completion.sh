c7b25d3f00e4fa83f16c7c0bd704fe8a4c7e9fdb
lancethomps <lthompson05@gmail.com>
2020-07-13 15:08:52 -0700

    add bash completion for install_git_hook and uninstall_git_hook

---
+++ dotfiles/.bash_completion.d/git-scripts-completion.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+function __complete_install_git_hook() {
+  local COMPREPLY_ADD
+  COMPREPLY=()
+
+  if command -v _allopt > /dev/null 2>&1; then
+    COMPLETE_NO_FILEDIR=true _allopt "$@"
+  fi
+
+  mapfile -t COMPREPLY_ADD < <(compgen -W "$(command "$1" --list)" -- "${COMP_WORDS[COMP_CWORD]}")
+  COMPREPLY+=("${COMPREPLY_ADD[@]}")
+}
+complete -o bashdefault -o default -F __complete_install_git_hook install_git_hook uninstall_git_hook

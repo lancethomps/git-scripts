798061f6c2d8877ed4a283d52234c53363ba5e12
lancethomps <lthompson05@gmail.com>
2023-07-26 21:01:21 -0700

    [git_commit_and_push_all] various updates

--- dotfiles/bin/.command_defaults.sh
+++ dotfiles/bin/.command_defaults.sh
@@ -8,6 +8,8 @@ source "${_SCRIPT_DIR}/.common_copy.sh"
 # shellcheck source=./.git_ci_types.sh
 source "${_SCRIPT_DIR}/.git_ci_types.sh"
 
+export GIT_URL_REGEX='(?i)^(?:(https|ssh|git\+ssh):\/\/)?([^@\/]+@)?([^\/:]+)(:[0-9]+)?(?:\/|:)(scm\/)?([^\/]+)\/([^\/]+?)(?:\.git)?(#[^\n]*)?$'
+
 function use_git_pager_if_set() {
   local gitpager
   if gitpager="$(git config core.pager)"; then
@@ -34,41 +36,32 @@ function urlencode_git_current_branch() {
   url_encode_py "${current_branch}" "/"
 }
 
-if test -z "${GIT_BRANCH_SKIP_REGEX-}"; then
-  export GIT_BRANCH_SKIP_REGEX='[[:space:]]((origin/)?(master|main|mature|HEAD|develop)$|origin/pr/)'
-fi
-
-GIT_BRANCH_REF_FORMATS=(
-  '%(align:50)%(committerdate:format:%Y-%m-%d %H:%M:%S) (%(color:green)%(committerdate:relative)%(color:reset))%(end)'
-  '%(color:red)%(objectname:short)%(color:reset)'
-  '%(align:40)%(color:blue)%(authorname)%(color:reset)%(end)'
-  '%(refname:short)'
-)
-GIT_BRANCH_REF_FORMATS_FULL=(
-  "${GIT_BRANCH_REF_FORMATS[@]}"
-  '%(contents:subject)'
-)
-
-GIT_BRANCH_REF_FORMAT=''
-for format in "${GIT_BRANCH_REF_FORMATS[@]}"; do
-  if test -n "${GIT_BRANCH_REF_FORMAT-}"; then
-    GIT_BRANCH_REF_FORMAT="${GIT_BRANCH_REF_FORMAT}%09"
+function git_branch_skip_regex() {
+  if test -z "${GIT_BRANCH_SKIP_REGEX-}"; then
+    export GIT_BRANCH_SKIP_REGEX='[[:space:]]((origin/)?(master|main|mature|HEAD|develop)$|origin/pr/)'
   fi
-  GIT_BRANCH_REF_FORMAT="${GIT_BRANCH_REF_FORMAT}${format}"
-done
+  echo "$GIT_BRANCH_SKIP_REGEX"
+}
 
-GIT_BRANCH_REF_FORMAT_FULL=''
-for format in "${GIT_BRANCH_REF_FORMATS_FULL[@]}"; do
-  if test -n "${GIT_BRANCH_REF_FORMAT_FULL-}"; then
-    GIT_BRANCH_REF_FORMAT_FULL="${GIT_BRANCH_REF_FORMAT_FULL}%09"
-  fi
-  GIT_BRANCH_REF_FORMAT_FULL="${GIT_BRANCH_REF_FORMAT_FULL}${format}"
-done
+function git_branch_ref_format() {
+  local format ref_format="" ref_formats=(
+    '%(align:50)%(committerdate:format:%Y-%m-%d %H:%M:%S) (%(color:green)%(committerdate:relative)%(color:reset))%(end)'
+    '%(color:red)%(objectname:short)%(color:reset)'
+    '%(align:40)%(color:blue)%(authorname)%(color:reset)%(end)'
+    '%(refname:short)'
+    "$@"
+  )
 
-GIT_TAG_REF_FORMAT="$GIT_BRANCH_REF_FORMAT"
-unset GIT_BRANCH_REF_FORMATS GIT_BRANCH_REF_FORMATS_FULL format
-export GIT_BRANCH_REF_FORMAT
-export GIT_TAG_REF_FORMAT
+  for format in "${ref_formats[@]}"; do
+    if test -n "${ref_format-}"; then
+      ref_format="${ref_format}%09"
+    fi
+    ref_format="${ref_format}${format}"
+  done
 
-export GIT_BRANCH_SKIP_SIZE_DEFAULT=10
-export GIT_URL_REGEX='(?i)^(?:(https|ssh|git\+ssh):\/\/)?([^@\/]+@)?([^\/:]+)(:[0-9]+)?(?:\/|:)(scm\/)?([^\/]+)\/([^\/]+?)(?:\.git)?(#[^\n]*)?$'
+  echo "$ref_format"
+}
+
+function git_branch_ref_format_full() {
+  git_branch_ref_format '%(contents:subject)'
+}
